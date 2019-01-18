2f13b5f162b42267e4bff5b00c3de64a14bcdb8c
Grisha Stetsenko <So4i94@gmail.com>
2019-01-18 21:52:26 +0200

    Custom Menu Bar using UICollectionView

--- youtube/AppDelegate.swift
+++ youtube/AppDelegate.swift
@@ -25,6 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         UINavigationBar.appearance().barTintColor = UIColor.rgb(red: 230, green: 32, blue: 31)
         
+        // get rid of black bar underneath navbar
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        
         let statusBarBackgroundView = UIView()
         statusBarBackgroundView.backgroundColor = UIColor.rgb(red: 194, green: 31, blue: 31)
         
--- youtube/View/VideoCell.swift
+++ youtube/View/VideoCell.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class VideoCell: UICollectionViewCell {
+class BaseCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
+    
+    func setupViews() {
+        
+    }
+        
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class VideoCell: BaseCell {
     
     let tumbnailImageView: UIImageView = {
         let imageView = UIImageView()
@@ -51,7 +62,7 @@ class VideoCell: UICollectionViewCell {
         return textView
     }()
     
-    func setupViews() {
+    override func setupViews() {
         addSubview(tumbnailImageView)
         addSubview(separatorView)
         addSubview(userProfileImageView)
@@ -81,9 +92,5 @@ class VideoCell: UICollectionViewCell {
         //heigth constraint
         addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 30))
         
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
